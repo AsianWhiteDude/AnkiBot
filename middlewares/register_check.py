2fdd1a3ab3be693978ca3384a2bb26ee32964098
Mikhail Kim <kimisharik@gmail.com>
2024-03-23 18:49:38 +0300

    Cached register_check middleware
    Took 32 minutes

--- middlewares/register_check.py
+++ middlewares/register_check.py
@@ -1,13 +1,11 @@
 from typing import Any, Awaitable, Callable, Dict, Union
-
 from aiogram import BaseMiddleware
 from aiogram.types import TelegramObject, Message, CallbackQuery
 from sqlalchemy import select
-
 from database.engine import sessionmaker
-
-
 from database.user import User
+from database.db_commands import is_user_exists
+from structures.redis import redis
 
 
 class register_check(BaseMiddleware):
@@ -19,22 +17,20 @@ class register_check(BaseMiddleware):
     ) -> Any:
 
         session_maker: sessionmaker = data['session_maker']
+
+        if is_user_exists(user_id=event.from_user.id, session_maker=session_maker, redis=redis):
+            return await handler(event, data)
+
         async with session_maker() as session:
             async with session.begin():
-                result = await session.execute(statement=select(User).where(User.user_id == event.from_user.id))
-                user = result.one_or_none()
-
-                if user:
-                    pass
+                user = User(
+                    user_id=event.from_user.id,
+                    username=event.from_user.username,
+                )
+                await session.merge(user)
+                if isinstance(event, Message):
+                    await event.answer('Ты был успешно зарегестрирован!')
                 else:
-                    user = User(
-                        user_id=event.from_user.id,
-                        username=event.from_user.username,
-                    )
-                    await session.merge(user)
-                    if isinstance(event, Message):
-                        await event.answer('Ты был успешно зарегестрирован!')
-                    else:
-                        await event.message.answer('Ты был успешно зарегестрирован!')
+                    await event.message.answer('Ты был успешно зарегестрирован!')
 
         return await handler(event, data)
